20487570e117e6424547c122d52696d05247366f
Sergo <sergofiles-16@mail.ru>
2025-02-05 13:40:48 +0300

    add localhost

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
-
-from app.api.routes import users, projects, auth
+import uvicorn
+import os
+# from api.routes import users, projects, auth
 
 app = FastAPI(title="Freelance Platform API", version="1.0.0")
 
-app.include_router(users.router, prefix="/users", tags=["Users"])
-app.include_router(projects.router, prefix="/projects", tags=["Projects"])
-app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+# app.include_router(users.router, prefix="/users", tags=["Users"])
+# app.include_router(projects.router, prefix="/projects", tags=["Projects"])
+# app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 
 @app.get("/")
 def root():
     return {"message": "Welcome to the Freelance Platform API"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=os.getenv("LOCALHOST"), port=os.getenv("PORT"))
