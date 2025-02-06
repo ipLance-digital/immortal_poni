12fdc514cbf4e3ae64912cbf8ce1259c237d92c9
pss <pss@roscluster.ru>
2025-02-06 18:10:28 +0300

    wip

--- app/main.py
+++ app/main.py
@@ -1,18 +1,22 @@
 from fastapi import FastAPI
 import uvicorn
 import os
-# from api.routes import users, projects, auth
+
 from api.routes import auth
+# from api.routes import users, projects, auth
 
 app = FastAPI(title="Freelance Platform API", version="1.0.0")
 
+# app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 # app.include_router(users.router, prefix="/users", tags=["Users"])
 # app.include_router(projects.router, prefix="/projects", tags=["Projects"])
-app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to the Freelance Platform API"}
+    return {"message": "Wellcome to IP-lance"}
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=os.getenv("LOCALHOST"), port=os.getenv("PORT"))
+    uvicorn.run(app, host=os.getenv("LOCALHOST"))
+
