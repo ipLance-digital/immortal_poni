bd74e8f4628cc319c92222a9a8bb6d19067d1d9c
pss <pss@roscluster.ru>
2025-02-06 18:58:13 +0300

    wip

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
 import os
-from app.api.routes import users
-from app.db.init_db import init_db
-
 
 app = FastAPI(title="Freelance Platform API", version="1.0.0")
 
@@ -11,15 +8,6 @@ app = FastAPI(title="Freelance Platform API", version="1.0.0")
 @app.get("/")
 def root():
     return {"message": "Wellcome to IP-lance"}
-
-
-@app.on_event("startup")
-async def startup_event():
-    init_db()
-
-
-app.include_router(users.router, prefix="/users", tags=["users"])
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host=os.getenv("LOCALHOST"))
