12e8081cd50059732f51dd03004e5604e50b1152
pss <pss@roscluster.ru>
2025-02-06 18:38:48 +0300

    wip

---
+++ app/database/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String
+from .session import Base
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+    name = Column(
+        String,
+        index=True
+    )
+    description = Column(
+        String
+    )
