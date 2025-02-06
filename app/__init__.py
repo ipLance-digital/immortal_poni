a8f0403cdeae9b2bb1b91bbe898a1390f762dba2
Artur <brevnishko2@gmail.com>
2025-02-06 22:11:53 +0300

    начальная реализация базы наколенке

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,1 @@
-from fastapi import FastAPI
 
-from app.routers import example_router  # Замените на реальные роутеры
-
-app = FastAPI(title="Freelance Platform API", version="1.0.0")
-
-app.include_router(example_router.router, prefix="/example", tags=["Example"])
-
-@app.get("/")
-def root():
-    return {"message": "Welcome to the Freelance Platform API"}
--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+import asyncpg
 from fastapi import FastAPI
 import uvicorn
 import os
+from database.postgres import UnitOfWork, User
+
 
 app = FastAPI(title="Freelance Platform API", version="1.0.0")
 
@@ -8,6 +11,25 @@ app = FastAPI(title="Freelance Platform API", version="1.0.0")
 @app.get("/")
 def root():
     return {"message": "Wellcome to IP-lance"}
+
+
+@app.get("/create_user")
+async def create_user():
+    connection = await asyncpg.connect(user='postgres', password='postgres',
+                                       database='test', host='127.0.0.1')
+    async with UnitOfWork(connection) as uow:
+        new_user = User(id=1, name='Alice')
+        await uow.user_repository.add_user(new_user)
+
+
+@app.get("/get_user")
+async def get_user():
+    connection = await asyncpg.connect(user='postgres', password='postgres',
+                                       database='test', host='127.0.0.1')
+    async with UnitOfWork(connection) as uow:
+        user = await uow.user_repository.get_user(1)
+    return {"user": user.name}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host=os.getenv("LOCALHOST"))
